4675403394e03c18629d46521db8116a587a8f98
Max Baak <Baak.Max@kpmg.nl>
2018-07-26 14:13:58 +0200

    Update to roofit_percentile_binning link
    - Added feature to set min and max values of the binning of a variable
    - In addition, for non-central binning the bin center is always set to the
      mean from data of a bin, not just for a RooDataSet as input, but now also
      for a DataFrame.

--- python/esroofit/links/roofit_percentile_binning.py
+++ python/esroofit/links/roofit_percentile_binning.py
@@ -22,9 +22,7 @@ import numpy as np
 import pandas as pd
 
 from eskapade import process_manager, Link, DataStore, StatusCode
-
 from esroofit import data_conversion, roofit_utils, RooFitManager
-
 
 class RooFitPercentileBinning(Link):
     """Evaluate percentile binning for given variable set."""
@@ -36,11 +34,18 @@ class RooFitPercentileBinning(Link):
         :param str read_key: key of input data to read from data store; either a RooDataSet or pandas DataFrame
         :param bool from_ws: if true, pick up input roodataset from workspace, not datastore (default is False)
         :param dict var_number_of_bins: number of percentile bins per observable
-        :param str binning_name: name of binning configuration to assign to percentile binning
+        :param dict var_binning: existing binning dict, to append binnings to.
+        :param str binning_name: name of binning configuration to assign to percentile binning. Also key for datastore.
+        :param dict var_min_value: min value for certain variable (optional)
+        :param dict var_max_value: max value for certain variable (optional)
+        :param float min_value: min value for all variables. Default is None (optional)
+        :param float max_value: max value for all variables. Default is None (optional)
+        :param bool non_central_binning: if false, use regular RooBinning class. Default is RooNonCentralBinning.
         """
         # initialize link and process arguments
         Link.__init__(self, kwargs.pop('name', 'RooFitPercentileBinning'))
-        self._process_kwargs(kwargs, read_key='', from_ws=False, binning_name='percentile', var_number_of_bins={})
+        self._process_kwargs(kwargs, read_key='', from_ws=False, binning_name='percentile', var_number_of_bins={}, var_binning={},
+                             var_min_value={}, var_max_value={}, min_value=None, max_value=None, non_central_binning=True)
         self.check_extra_kwargs(kwargs)
 
     def initialize(self):
@@ -69,17 +74,8 @@ class RooFitPercentileBinning(Link):
                 raise KeyError('Key "{}" not found in datastore.'.format(self.read_key))
             data = ds[self.read_key]
 
-        # check presence of all columns
-        columns = list(self.var_number_of_bins)
-        for col in columns:
-            var = ws.var(col)
-            if not var:
-                raise RuntimeError('no variable with key "{}" in workspace.'.format(col))
-            if not isinstance(var, ROOT.RooRealVar):
-                self.logger.warning('Variable "{var}" is not a RooRealVar; rejecting.', var=col)
-                del self.var_number_of_bins[col]
-
-        # check data type
+        # check presence of all columns; check data type
+        columns = list(self.var_number_of_bins.keys())
         if isinstance(data, ROOT.RooDataSet):
             if data.numEntries() == 0:
                 raise AssertionError('RooDataSet "{}" is empty.'.format(self.read_key))
@@ -87,14 +83,14 @@ class RooFitPercentileBinning(Link):
             for col in self.var_number_of_bins:
                 if not varset.find(col):
                     raise AssertionError('Column "{}" not found in input roodataset.'.format(col))
-            df = data_conversion.rds_to_df(data, list(self.var_number_of_bins), ignore_lost_records=True)
+            df = data_conversion.rds_to_df(data, columns, ignore_lost_records=True)
         elif isinstance(data, pd.DataFrame):
             if len(data.index) == 0:
                 raise AssertionError('RooDataSet "{}" is empty.'.format(self.read_key))
             for col in self.var_number_of_bins:
                 if col not in data.columns:
                     raise AssertionError('Column "{}" not found in input dataframe.'.format(col))
-            df = data[list(self.var_number_of_bins.keys())]
+            df = data[columns]
         else:
             raise TypeError('Object "{0:s}" not of type RooDataSet/DataFrame (got "{1!s}").'
                             .format(self.read_key, type(data)))
@@ -103,21 +99,52 @@ class RooFitPercentileBinning(Link):
         # evaluate and set quantiles per observable
         for col, nbins in self.var_number_of_bins.items():
             self.logger.debug('Creating {n:d} percentile bins for column "{col}".', n=nbins, col=col)
-            # column variable is a RooRealVar
             qs = np.linspace(0, 1, nbins + 1).tolist()
-            bin_edges = df[col].quantile(qs)
-            binning = ROOT.RooNonCentralBinning() if isinstance(data, ROOT.RooDataSet) else ROOT.RooBinning()
-            col_var = ws.var(col)
-            binning.setRange(col_var.getMin(), col_var.getMax())
-            for b in bin_edges.values[1:nbins]:
-                if col_var.getMin() < b < col_var.getMax():
-                    binning.addBoundary(b)
-            if isinstance(data, ROOT.RooDataSet):
-                binning.setAverageFromData(data, col_var)
-            col_var.setBinning(binning, self.binning_name)
+            bin_edges = df[col].quantile(qs).values
+            # column variable is a RooRealVar
+            col_var = ws.var(col) if ws.var(col) else data.get(0).find(col) if isinstance(data, ROOT.RooDataSet) else None
+            if col_var and not ws.var(col):
+                # add variables to workspace if they don't exist there yet. useful for consistent use.
+                ws.put(col_var)
+                col_var = ws.var(col)
+            if col_var:
+                # determine if variable has predefined range, that is also not infinity
+                range_min = col_var.getMin() if col_var.getMin() != -ROOT.RooNumber.infinity() else None
+                range_max = col_var.getMax() if col_var.getMax() != ROOT.RooNumber.infinity() else None
+                # if range exists, use it to *replace* outer quantiles if further away
+                # this avoids min and max quantiles very close to edges of range
+                if range_min and range_min < bin_edges[0]:
+                    bin_edges[0] = range_min
+                if range_max and range_max > bin_edges[-1]:
+                    bin_edges[-1] = range_max
+            # has a manual range been set? if so, overwrite existing range
+            # this means an update the final bin_edges - may have changed b/c of user var_min and var_max
+            # i.e. no guarantuee then that first and last bin are correct quantiles
+            var_min = self.var_min_value.get(col, self.min_value if self.min_value else bin_edges[0])
+            var_max = self.var_max_value.get(col, self.max_value if self.max_value else bin_edges[-1])
+            if col_var:
+                col_var.setRange(var_min, var_max)
+            binning = ROOT.RooNonCentralBinning() if self.non_central_binning else ROOT.RooBinning()
+            binning.setRange(var_min, var_max)
+            for i in range(0, nbins+1):
+                bv = bin_edges[i]
+                binning.addBoundary(bv)
+            if self.non_central_binning:
+                # evaluate average value of each bin, from data
+                for i in range(binning.numBins()):
+                    keep = df[col].between(binning.binLow(i), binning.binHigh(i))
+                    bc = df[keep][col].mean()
+                    binning.setBinCenter(i, bc)
+            # try adding binning to corresponding variables in workspace
+            if col_var:
+                col_var.setBinning(binning, self.binning_name)
+            self.var_binning[col] = binning
 
         # cleanup of temporary df
         if isinstance(data, ROOT.RooDataSet):
             del df
+
+        # storage
+        ds[self.binning_name] = self.var_binning
 
         return StatusCode.Success
